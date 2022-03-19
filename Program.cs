01f273fc36819120da9f8c428fbfaf564eb62817
realizyy <realizy@student.telkomuniversity.ac.id>
2022-03-19 12:00:45 +0800

    Ver 3, menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+using modul4_1302204035;
+
 Console.WriteLine("Hello, World!");
+SimpleDataBase<int, DateTime> db = new SimpleDataBase<int, DateTime>();
+db.AddData(00, DateTime.Now);//Data 0
+db.AddData(13, DateTime.UtcNow);
+db.AddData(02, DateTime.UtcNow);
+db.AddData(20, DateTime.UtcNow);
+db.PrintAllData();
---
+++ SimpleDataBase.cs
@@ -0,0 +1,27 @@
+﻿namespace modul4_1302204035;
+
+public class SimpleDataBase<T,Date>
+{
+    private List<T> storedData;
+    private List<Date> storedDate;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        storedDate = new List<Date>();
+    }
+    
+    public void AddData(T data, Date date)
+    {
+        storedData.Add(data);
+        storedDate.Add(date);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 1; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + i + " berisi : " + storedData[i] + ", yang disimpan pada waktu UTC:" + storedDate[i]);
+        }
+    }
+}
